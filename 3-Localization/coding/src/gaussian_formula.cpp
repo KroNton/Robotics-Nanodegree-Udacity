cb8fd1779b234532a5a87f606b28dbb5c9b65fec
kyrillos Fekry <kyrlosfekry@gmail.com>
2020-10-21 01:13:30 +0200

    adding coding package & writing gaussian formula in cpp

---
+++ 3-Localization/coding/src/gaussian_formula.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+
+
+double f(double mu,double sigma2, double x)
+{
+double sigma=sqrt(sigma2);
+
+   double prob=(1/(sigma*sqrt(2*M_PI)))*exp((-0.5)*pow((x-mu)/sigma,2.0));
+
+
+   return prob;
+
+}
+
+int main()
+{
+
+std::cout<<f(10.0,4.0,8.0)<<std::endl;
+   return 0;
+
+}
